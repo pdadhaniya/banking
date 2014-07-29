f6fce61249efc20cedfee240af597778680649b2
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-07-29 10:56:32 -0500

    add initialize method for bank class

--- banking.rb
+++ banking.rb
@@ -2,6 +2,7 @@
 
 class Person #made the Person class
 
+	attr_reader :name
 	attr_reader :cash_on_hand
 
 	def initialize(name, cash_on_hand=0)
@@ -12,10 +13,14 @@ class Person #made the Person class
 end
 
 
-
-
-
 class Bank #made the Bank class
 
+	attr_reader :bank_name
+	attr_reader :total_cash
+
+	def initialize(bank_name, total_cash=0)
+		@bank_name = bank_name
+		@total_cash = total_cash
+	end
 
 end
