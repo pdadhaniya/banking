549b3d33c9ccb0d8e64365f81498e93b5cc88911
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-07-29 11:23:39 -0500

    update bank initialize with empty hash for accounts and update open account method to consider if account already exists

--- banking.rb
+++ banking.rb
@@ -17,14 +17,22 @@ class Bank #made the Bank class
 
 	attr_reader :bank_name
 	attr_reader :total_cash
+	attr_reader :accounts
 
 	def initialize(bank_name, total_cash=0)
 		@bank_name = bank_name
 		@total_cash = total_cash
+		@accounts = Hash.new
 	end
 
 	def open_account(account_name)
+		if @accounts[account_name]
+			puts "#{account_name.name} already has an account at #{bank_name}! Can't have two!!"
+		else
+		@accounts[account_name.name] = 0
 		puts "#{account_name.name}, thanks for opening an account at #{@bank_name}!"
+		puts "#{account_name.name}'s account at #{@bank_name} is open with $#{@accounts[account_name.name]} saved!"
+		end
 	end
 
 end
